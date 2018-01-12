2b53419eafab368ddd1bc7acdd48ce057e4104c9
Jonathan Shuai <jonathanshuai0@gmail.com>
2018-01-12 13:40:47 -0800

    set up the pipeline for creating embeddings of faces using facenet

--- project/real_recognition.py
+++ project/real_recognition.py
@@ -1,6 +1,7 @@
 import os
 import time 
 import warnings
+import subprocess
 
 import numpy as np
 import pandas as pd
@@ -29,26 +30,14 @@ from FacePreprocessor import FacePreprocessor
 face_landmark_dat = "./models/shape_predictor_68_face_landmarks.dat"
 input_dir = "./images"
 output_dir = "./aligned"
+input_dir_len = len(input_dir) + 1
 
-jisoo_id = "js"
-seolhyun_id = "sh"
-iu_id = "iu"
-mf_id = "mf"
+openface_path = "../building/openface/batch-represent/main.lua"
+openface_outdir =  "-outDir"
+openface_outdir_arg = "./reps" 
+openface_data_dir = "-data"
+openface_data_dir_arg = "./aligned"
 
-
-#Get only Jisoo and Seolhyun images
-#df = df.loc[(df['id'] == jisoo_id) | (df['id'] == seolhyun_id)]
-#df = df.loc[(df['id'] == jisoo_id)].head(4)
-#df = df.loc[(df['id'] == seolhyun_id)]
-#df = df.loc[(df['path'] == './images/jisoo/jisoo13.jpg')]
-#df = df.loc[(df['path'] == './iu/iu1.jpg')]
-#df = df.loc[(df['id'] == jisoo_id) | (df['id'] == iu_id)]
-#df = df.loc[(df['id'] == iu_id)]
-#df = df.loc[(df['id'] == mf_id) | (df['id'] == iu_id)]
-
-print(os.path.join(subdir, file))
-        print(subdir)
-        print(file)
 parameters = {'landmark_dat': face_landmark_dat, 
 'left_eye_pos': (0.37, 0.37), 'lip_pos': 0.82, 'width': 200, 'height': 200}
 
@@ -59,30 +48,33 @@ if not os.path.exists(output_dir):
 
 for subdir, dirs, files in os.walk(input_dir):
   for file in files:
-	  #Read in the image
-	  input_image_file = os.path.join(subdir, file)
-	  image_color = cv2.imread(input_image_file)
-	  
-	  #Crop and align the face from the image (returns just ONE face (the largest))
-	  image_cropped = fp.crop_and_align(image_color)
+    #Read in the image
+    input_image_file = os.path.join(subdir, file)
+    image_color = cv2.imread(input_image_file)
 
-	  if image_cropped is None:
-	    warnings.warn('No face found for {}'.format(image_path), UserWarning)
+    #Crop and align the face from the image (returns just ONE face (the largest))
+    image_cropped = fp.crop_and_align(image_color)
 
+    #If we couldn't find an image, give a warning and continue
+    if image_cropped is None:
+      warnings.warn('No face found for {}'.format(image_path), UserWarning)
+      continue
 
-	  #Add the processed face and label to our list
-	  #processed_faces.append(image_cropped.flatten())
-	  #face_labels.append(row['label'])
-	  output_image_file = os.path.join(output_dir, input_image_file[len(input_dir):])
-	  output_image_dir = os.path.dirname(output_image_file)
-	  if not os.path.exists(output_image_dir):
-	  	os.makedirs(output_image_dir)
+    #Get/create the output directory and filename
+    output_image_file = os.path.join(output_dir, input_image_file[input_dir_len:])
+    output_image_dir = os.path.dirname(output_image_file)
+    if not os.path.exists(output_image_dir):
+    	os.makedirs(output_image_dir)
 
+    #Show the cropped image
+    #cv2.imshow("image_smoothed", image_cropped)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
 
-	  #cv2.imshow("image_smoothed", image_cropped)
-	  #cv2.waitKey(0)
-	  #cv2.destroyAllWindows()
+    #Write the image to the designated diretory
+    cv2.imwrite(output_image_file, image_cropped)
 
-	  cv2.imwrite(output_image_file, image_cropped)
+subprocess.call([openface_path, openface_outdir, openface_outdir_arg, 
+                openface_data_dir, openface_data_dir_arg])
 
 
